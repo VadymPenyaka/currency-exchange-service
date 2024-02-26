f29a9aca26a111433ae1e3fc79dacb35a115cb9b
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2024-02-26 13:21:10 +0200

    delete CurrencyRateScheduler class add CurrencyMapper

--- src/main/java/projects/currency_exchange_service/service/CurrencyServiceImpl.java
+++ src/main/java/projects/currency_exchange_service/service/CurrencyServiceImpl.java
@@ -3,9 +3,9 @@ package projects.currency_exchange_service.service;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import projects.currency_exchange_service.model.CurrencyDTO;
 
 import java.io.IOException;
@@ -17,34 +17,21 @@ import java.net.URL;
 import java.text.DecimalFormat;
 import java.util.*;
 import com.google.gson.JsonArray;
+import projects.currency_exchange_service.repository.CurrencyRepository;
 
 @Service
+@RequiredArgsConstructor
 public class CurrencyServiceImpl implements  CurrencyService {
     private static final String NBU_RATE_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
 
     private final double DEFAULT_MARKUP_PERCENT = 3;
-    private static HttpURLConnection httpURLConnection;
-    private static final DecimalFormat decfor = new DecimalFormat("0.00");
 
-    private static URL url;
-
-    static {
-        try {
-            url = new URL(NBU_RATE_URL);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            httpURLConnection = (HttpURLConnection) url.openConnection();// connection to NBU API
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private final CurrencyRepository currencyRepository;
 
 
-    private JsonArray getCurrenciesInJsonArray (HttpURLConnection httpURLConnection) throws IOException {
+    private JsonArray getCurrenciesInJsonArray () throws IOException {
 
-        JsonElement root = JsonParser.parseReader(new InputStreamReader((InputStream) httpURLConnection.getContent()));
+        JsonElement root = JsonParser.parseReader(new InputStreamReader((InputStream) getConnection().getContent()));
         JsonArray jsonArray = root.getAsJsonArray();
         System.out.println(jsonArray);
 
@@ -73,26 +60,28 @@ public class CurrencyServiceImpl implements  CurrencyService {
 
 
 
-//    private HttpURLConnection getConnection () throws IOException {
-//        URL url = new URL(NBU_RATE_URL);
-//        HttpURLConnection request = (HttpURLConnection) url.openConnection();
-//        return request;
-//    }
+    private HttpURLConnection getConnection () throws IOException {
+        URL url = new URL(NBU_RATE_URL);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        return request;
+    }
 
 
 
 
     @Override
-    public List<CurrencyDTO> getAllCurrencies() throws IOException {
+    public void updateAllCurrencies() throws IOException {
+        currencyRepository.deleteAll();
         List<CurrencyDTO> currencies = new ArrayList<>();
-        JsonArray jsonArray = getCurrenciesInJsonArray(httpURLConnection);
+        JsonArray jsonArray = getCurrenciesInJsonArray();
 
         for (int i =0; i<jsonArray.size(); i++ ) {
             JsonElement element= jsonArray.get(i);
             JsonObject jsonObject = element.getAsJsonObject();
             currencies.add(convertNBUJsonObjectToCurrencyDTO(jsonObject));
         }
-        return currencies;
+
+//        currencyRepository.saveAllAndFlush(currencies);
     }
 
     @Override
